e6508eee5f1d4c1006128b2d7e9f6492bd0e2510
valeriast <salinastejadaemmavaleria@gmail.com>
2025-05-14 11:39:06 -0600

    directorio prt2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hola");
+//En js tenemos las funciones de tipo felcha
+//Utilizaremos fetch que es un metodo para consumir una API
+
+//Funcion flecha
+const consumirApi = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    //convertimos la respuesta en tipo JSON
+    .then((response) => response.json()) 
+    //Los datos los vamos a mandar a la consola
+    .then((data) => console.log (data))
+//descubrir que hacer en caso de que no me corresponda
+.catch((error) => console.log(error));
+};
+
+consumirApi()
